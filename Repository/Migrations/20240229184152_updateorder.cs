3981317293eae3597d3954e2fc8e4a41dadae50a
bachdoan-sa <bach.doansy@gmail.com>
2024-03-01 01:42:38 +0700

    add migration config order and order detail

---
+++ Repository/Migrations/20240229184152_updateorder.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateorder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderDetails_Orders_OrderId",
+                table: "OrderDetails");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_OrderDetails_OrderDetailId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_OrderDetailId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderDetails_OrderId",
+                table: "OrderDetails");
+
+            migrationBuilder.DropColumn(
+                name: "OrderDetailId",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OrderId",
+                table: "OrderDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OrderDetailStatus",
+                table: "OrderDetails",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_OrderDetailStatus",
+                table: "OrderDetails",
+                column: "OrderDetailStatus");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderDetails_Orders_OrderDetailStatus",
+                table: "OrderDetails",
+                column: "OrderDetailStatus",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderDetails_Orders_OrderDetailStatus",
+                table: "OrderDetails");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderDetails_OrderDetailStatus",
+                table: "OrderDetails");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OrderDetailId",
+                table: "Orders",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OrderId",
+                table: "OrderDetails",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OrderDetailStatus",
+                table: "OrderDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_OrderDetailId",
+                table: "Orders",
+                column: "OrderDetailId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_OrderId",
+                table: "OrderDetails",
+                column: "OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderDetails_Orders_OrderId",
+                table: "OrderDetails",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_OrderDetails_OrderDetailId",
+                table: "Orders",
+                column: "OrderDetailId",
+                principalTable: "OrderDetails",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
