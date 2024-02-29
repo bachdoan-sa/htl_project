b94127e3ea731fb62f529fd5f97029c30d452094
bachdoan-sa <bach.doansy@gmail.com>
2024-03-01 03:05:43 +0700

    add profile user

---
+++ WebHTL/Pages/ProfileAccount.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebHTL.Pages
+{
+    public class ProfileAccountModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebHTL/Pages/ProfileProduct.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebHTL.Pages
+{
+    public class ProfileProductModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
