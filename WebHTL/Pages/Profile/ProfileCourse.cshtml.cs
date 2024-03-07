ef2c84c9b1f421f7d05220cafefab5d52fada33d
bachdoan-sa <bach.doansy@gmail.com>
2024-03-08 01:51:58 +0700

    add roadmap for user

---
+++ WebHTL/Pages/Profile/ProfileCourse.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebHTL.Pages.Profile
+{
+    public class ProfileCourseModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebHTL/Pages/Profile/ProfileProduct.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebHTL.Pages.Profile
+{
+    public class ProfileProductModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
