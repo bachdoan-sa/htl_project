02a442395e2c1f68901ef6b708cd1a8062779f63
Dangkhoa305 <dangkhoa1699@gmail.com>
2024-03-08 03:45:54 +0700

    thêm course page

---
+++ WebHTL/Pages/CoursePage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebHTL.Pages
+{
+    public class CoursePageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
