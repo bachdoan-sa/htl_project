6f591e3c6090bab87c7fd8729d75e8742d2a3d60
AnhKhoa <nguyenanhkhoa1699@gmail.com>
2024-03-12 20:09:43 +0700

    fix CoursePage

--- WebHTL/Pages/CoursePage.cshtml.cs
+++ WebHTL/Pages/CoursePage.cshtml.cs
@@ -9,18 +9,33 @@ namespace WebHTL.Pages
     public class CoursePageModel : PageModel
     {
         private readonly ICourseModuleService _courseModuleService;
+        private readonly ICourseLessonService _courseLessonService;
 
-        public CoursePageModel(ICourseModuleService courseModuleService)
+        public CoursePageModel(ICourseModuleService courseModuleService, ICourseLessonService courseLessonService)
         {
             _courseModuleService = courseModuleService;
+            _courseLessonService = courseLessonService;
         }
         public List<CourseModuleModel> CourseModules { get; private set; }
+        public CourseLessonModel Lesson { get; private set; }
 
         public async Task<IActionResult> OnGetAsync(string courseId)
         {
             string a = "1";
             CourseModules = await _courseModuleService.GetByCourseId(a);
+
+            if (CourseModules != null && CourseModules.Any())
+            {
+                var firstModule = CourseModules.FirstOrDefault();
+                if (firstModule != null && firstModule.CourseLessons != null && firstModule.CourseLessons.Any())
+                {
+                    string lessonId = firstModule.CourseLessons.First().Id; // S? d?ng ID c?a bài h?c ??u tiên
+                    Lesson = await _courseLessonService.GetCourseLessonByCourseModuleId(lessonId);
+                }
+            }
             return Page();
         }
+
+        
     }
 }
