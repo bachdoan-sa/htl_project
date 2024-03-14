983723ff6754eaf127aae0e8e953edb3ee2fba77
Dangkhoa305 <dangkhoa1699@gmail.com>
2024-03-15 03:44:26 +0700

    fix fe profileRoadMap

--- WebHTL/Pages/Profile/ProfileRoadMap.cshtml.cs
+++ WebHTL/Pages/Profile/ProfileRoadMap.cshtml.cs
@@ -1,19 +1,49 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Repository.Model;
+using Repository.Services.IServices;
 
 namespace WebHTL.Pages.Profile
 {
     public class ProfileRoadMapModel : PageModel
     {
-        public IActionResult OnGet()
+        private readonly IRoadmapService _roadmapService;
+
+        public GetRoadmapDetailResDto Roadmap { get; set; }
+
+        public ProfileRoadMapModel(IRoadmapService roadmapService)
         {
-            var userId = HttpContext.Session.GetString("customerId");
+            _roadmapService = roadmapService;
+        }
+
+        //public IActionResult OnGet()
+        //{
+        //    var userId = HttpContext.Session.GetString("customerId");
+
+        //    if (userId == null)
+        //    {
+        //        return RedirectToPage("/SignIn");
+        //    }
+        //    return Page();
+        //}
+
+        public IActionResult OnGet(string id)
+        {
+            var userId = 1;
 
             if (userId == null)
             {
                 return RedirectToPage("/SignIn");
             }
-            return Page();
+            else
+            {
+                
+                return Page();
+            }
         }
+
+
+
+
     }
 }
