063b1a0c9cc1a3a1381fe81eabd8d3521e6a1135
Dangkhoa305 <dangkhoa1699@gmail.com>
2024-03-22 03:06:17 +0700

    update lesson Admin

--- WebHTL/Areas/Admin/Pages/CourseLesson/Update.cshtml.cs
+++ WebHTL/Areas/Admin/Pages/CourseLesson/Update.cshtml.cs
@@ -1,17 +1,52 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Repository.Model;
+using Repository.Services.IServices;
 
 namespace WebHTL.Pages.Areas.Admin.CourseLesson
 {
     public class UpdateModel : PageModel
     {
-        public IActionResult OnGet()
+        private readonly ICourseLessonService _courseLessonService;
+
+        public UpdateModel(ICourseLessonService courseLessonService)
         {
+            _courseLessonService = courseLessonService;
+        }
+
+        [BindProperty]
+        public CourseLessonModel CourseLesson { get; set; }
+
+        public async Task<IActionResult> OnGet(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
             if (HttpContext.Session.GetString("Admin") == null)
             {
                 return RedirectToPage("/SignIn");
             }
+            CourseLesson = await _courseLessonService.GetById(id);
+
+            if (CourseLesson == null)
+            {
+                return NotFound();
+            }
+
             return Page();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            await _courseLessonService.Update(CourseLesson);
+
+            return RedirectToPage("./Index"); 
         }
     }
 }
