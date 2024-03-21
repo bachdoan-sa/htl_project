d2ce5b3f46712dead5ca01841bf367ef993b1744
Dangkhoa305 <dangkhoa1699@gmail.com>
2024-03-21 23:45:28 +0700

    link trang Profile Product -> Roadmap -> course

--- WebHTL/Pages/Profile/ProfileCourse.cshtml.cs
+++ WebHTL/Pages/Profile/ProfileCourse.cshtml.cs
@@ -1,19 +1,54 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Repository.Model;
+using Repository.Services.IServices;
 
 namespace WebHTL.Pages.Profile
 {
     public class ProfileCourseModel : PageModel
     {
-        public IActionResult OnGet()
-        {
-            var userId = HttpContext.Session.GetString("customerId");
+        private readonly ICourseModuleService _courseModuleService;
+        private readonly ICourseLessonService _courseLessonService;
+        private readonly ICourseService _courseService;
 
-            if (userId == null)
+
+        public ProfileCourseModel(ICourseModuleService courseModuleService, ICourseLessonService courseLessonService, ICourseService courseService)
+        {
+            _courseModuleService = courseModuleService;
+            _courseLessonService = courseLessonService;
+            _courseService = courseService;
+        }
+        public List<CourseModuleModel> CourseModules { get; private set; }
+        public CourseLessonModel Lesson { get; private set; }
+
+        [BindProperty(Name = "CourseLessonId")]
+        public string CourseLessonId { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string CourseId, string CourseLessonId, string CourseModulesId)
+        {
+            string a = "1"; // can sua sau nay'
+            CourseModules = await _courseModuleService.GetByCourseId(CourseId);
+
+
+            if (CourseId == null)
             {
-                return RedirectToPage("/SignIn");
+                CourseId = (await _courseService.GetAll()).FirstOrDefault()?.Id;
             }
+
+            if (CourseModulesId == null)
+            {
+                CourseModulesId = (await _courseModuleService.GetByCourseId(CourseId)).FirstOrDefault()?.Id;
+            }
+
+            if (CourseLessonId == null)
+            {
+                CourseLessonId = (await _courseLessonService.GetCourseLessonByCourseModuleId(CourseModulesId))?.Id;
+            }
+
+            Lesson = await _courseLessonService.GetById(CourseLessonId);
             return Page();
         }
+
+
     }
 }
