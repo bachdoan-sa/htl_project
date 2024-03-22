b775256e5cac788f6663b85a5f85f535b6129523
bachdoan-sa <bach.doansy@gmail.com>
2024-03-22 08:11:32 +0700

    add thanh toan

--- WebHTL/Pages/Profile/Checkout.cshtml.cs
+++ WebHTL/Pages/Profile/Checkout.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -13,51 +14,15 @@ namespace WebHTL.Pages.Profile
 {
     public class CheckoutModel : PageModel
     {
-        private readonly ITransactionService _transactionService;
-        // Assume other services are injected as needed
-
-        [BindProperty]
-        public TransactionModel Transaction { get; set; } = default!;
-
-        private readonly IOrderDetailService _orderDetailService;
-        public List<OrderDetailModel> OrderDetails { get; set; } = default!;
-        public decimal TotalAmount { get; set; }
-
-        public CheckoutModel(ITransactionService transactionService, IOrderDetailService orderDetailService)
+        
+        public IActionResult OnGet(string items)
         {
-            _transactionService = transactionService;
-            _orderDetailService = orderDetailService;
+          
+            return Page();
         }
-
-        public async Task<IActionResult> OnGet(string items)
+/*
+        public IActionResult OnPostAsync()
         {
-            if (HttpContext.Session.GetString("customerId") == null)
-            {
-                return RedirectToPage("/SignIn");
-            }
-            var itemIds = items?.Split(',') ?? new string[0];
-
-            OrderDetails = await _orderDetailService.GetOrderDetailsByIds(itemIds);
-
-            TotalAmount = OrderDetails.Sum(od => od.Quantity * od.Price);
-
-            if (TotalAmount > 0 && OrderDetails.Any())
-            {
-                return Page();
-            }
-            else
-            {
-                TempData["ErrorMessage"] = "Your shopping cart is empty or an error occurred.";
-                return RedirectToPage("/Profile/ShoppingCart");
-            }
-        }
-
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
 
             // Process the payment based on the selected method
             if (Transaction.PaymentMethod == "Bank")
@@ -71,6 +36,6 @@ namespace WebHTL.Pages.Profile
 
             // Redirect to a confirmation page or display a success message
             return RedirectToPage("/PaymentConfirmation");
-        }
+        }*/
     }
 }
